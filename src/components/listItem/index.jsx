8b54cfeed7ca89107620746a43f1a7ab1e3f7f49
jessicarolinefr <jessicarolinefr@gmail.com>
2020-07-06 09:26:38 -0300

    fix list cursor

--- src/components/listItem/index.jsx
+++ src/components/listItem/index.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const ListItem = styled.li`
   align-items: center;
   border-bottom: 1px solid #2d2d2d;
+  cursor: pointer;
   font-weight: 700;
   margin-top: 10px;
   padding: 5px;
